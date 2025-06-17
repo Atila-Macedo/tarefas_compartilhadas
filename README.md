caa481055a8a538ed758b61e4167279a66668026
Claudio Santarem <claudiosantarem@bemol.com.br>
2025-06-17 12:47:43 -0400

    feat: configurando firebase realtime

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # seu_app_de_tarefas
 
 A new Flutter project.
+
+# Documentação do FireBase
+
+- https://firebase.flutter.dev/
+- https://firebase.flutter.dev/docs/overview
+
+Passo a Passo:
+- https://firebase.flutter.dev/docs/cli
+
+Instalando NodeJs:
+- https://nodejs.org/ptdart pub global activate flutterfire_cli
+
+Instalando o FlutterFireCLI
+- dart pub global activate flutterfire_cli
+
+Testes para ver se funcionou
+- firebase -V
+- flutterfire -v 
+
+Executar o FlutterFire 
+- flutterfire configure
+
+Instalando bibliotecas importantes:
+- firebase_core
--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'screens/auth/login_screen.dart';
-import 'utils/app_routes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:seu_app_de_tarefas/tarefas_app.dart';
+import 'package:seu_app_de_tarefas/firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const TarefasApp());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'App de Tarefas',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF6750A4),
-          brightness: Brightness.light,
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          filled: true,
-          fillColor: Colors.grey[100],
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide.none,
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide.none,
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: const BorderSide(color: Color(0xFF6750A4), width: 2),
-          ),
-          errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: const BorderSide(color: Colors.red, width: 2),
-          ),
-          contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: const Color(0xFF6750A4),
-            foregroundColor: Colors.white,
-            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
-          ),
-        ),
-        textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(
-            foregroundColor: const Color(0xFF6750A4),
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          ),
-        ),
-      ),
-      initialRoute: AppRoutes.login,
-      routes: AppRoutes.routes,
-    );
-  }
-} 
